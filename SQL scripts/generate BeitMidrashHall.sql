6cf8178875f750095920ed9fedfc935783d65593
YurishoSan <yurisho@gmail.com>
2015-05-13 22:45:22 +0300

    Continued Populating DB
    * fixed Rabbi.csv
    - removed Rabbies.xlsx
    + created RavDef.def, and added rabbies to DB (export TBA)
    * fixed Student.csv
    TODO: popilate db with students.
    * updated BeitMidrashHall.sql - still not working.

--- SQL scripts/generate BeitMidrashHall.sql
+++ SQL scripts/generate BeitMidrashHall.sql
@@ -1,8 +1,8 @@
-INSERT INTO BeitMidrashHall
-VALUES (0, 'Havaad Haleum 21, Jerusalem', 'Rosenberg Hall', 100, TRUE, 3, 0);
+INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES ('Havaad Haleum 21, Jerusalem', 'Rosenberg Hall', 100, '1', 3, 205465212);
 
-INSERT INTO BeitMidrashHall
-VALUES (1, 'Havaad Haleum 22, Jerusalem', 'Dasht Hall', 150, FALSE, 5, 1);
+INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES ('Havaad Haleum 22, Jerusalem', 'Dasht Hall', 150, '0', 5, 258659456);
 
-INSERT INTO BeitMidrashHall
-VALUES (0, 'Havaad Haleum 23, Jerusalem', 'Frumshtein Hall', 50, TRUE, 1, 3);
+INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES ('Havaad Haleum 23, Jerusalem', 'Frumshtein Hall', 50, 1, '1', 154126548);
