ed77caae564bb659314fb3e4f543f352855a09fe
YurishoSan <yurisho@gmail.com>
2015-05-20 17:01:03 +0300

    Generated last tables, database export
    + csv files and def files.
    + beitMidrashHall.sql fixed FINAL.

--- SQL scripts/generate BeitMidrashHall.sql
+++ SQL scripts/generate BeitMidrashHall.sql
@@ -1,8 +1,8 @@
-INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
-VALUES ('Havaad Haleum 21, Jerusalem', 'Rosenberg Hall', 100, '1', 3, 205465212);
+INSERT INTO BeitMidrashHall (BMHID,Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES (0,'Havaad Haleum 21, Jerusalem', 'Rosenberg Hall', 100, '1', 3, 205465212);
 
-INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
-VALUES ('Havaad Haleum 22, Jerusalem', 'Dasht Hall', 150, '0', 5, 258659456);
+INSERT INTO BeitMidrashHall (BMHID,Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES (1,'Havaad Haleum 22, Jerusalem', 'Dasht Hall', 150, '0', 5, 258659456);
 
-INSERT INTO BeitMidrashHall (Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
-VALUES ('Havaad Haleum 23, Jerusalem', 'Frumshtein Hall', 50, 1, '1', 154126548);
+INSERT INTO BeitMidrashHall (BMHID,Address,Name,Capacity,Womensection,Torahbooksnum,Ravid)
+VALUES (2,'Havaad Haleum 23, Jerusalem', 'Frumshtein Hall', 50, '1', 1, 154126548);
