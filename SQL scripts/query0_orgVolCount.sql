46dc991deb027043189a787ac9d109030f367cc7
YurishoSan <yurisho@gmail.com>
2015-06-02 18:37:47 +0300

    queries 4 and 0 (this time for real)
    + added query 4
    * fixed query 0

--- SQL scripts/query0_orgVolCount.sql
+++ SQL scripts/query0_orgVolCount.sql
@@ -1,2 +1,4 @@
+WITH orgCountTbl AS (SELECT ravid, organization, COUNT(organization) AS orgCount FROM Student INNER JOIN Volunteergroup ON Student.Vgid = Volunteergroup.Vgid GROUP BY ravid, organization) 
+SELECT organization, Rav.ravid, Ravname, orgCount FROM Rav INNER JOIN orgCountTbl
+ON Rav.ravid = orgCountTbl.ravid
 ORDER BY organization
-
