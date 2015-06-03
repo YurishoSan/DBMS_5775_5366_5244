1c5f117984fc64f5832bc4cf0b89692568da5a94
YurishoSan <yurisho@gmail.com>
2015-06-03 18:47:53 +0300

    Change queries As requested
    * queries reordered from easy to hard
    * Plain text update.
    * fixed query 1 plain text (in new scheme) to fit actual query.

--- SQL scripts/query3_avgAgePerBMH.sql
+++
@@ -1,4 +0,0 @@
-SELECT BeitMidrashHall.bmhID, BeitMidrashHall.name, AVG(MONTHS_BETWEEN(SYSDATE, dateOfBirth) / 12) AS abvAge FROM BeitMidrashHall INNER JOIN Student
-ON Student.bmhID = BeitMidrashHall.bmhID
-GROUP BY BeitMidrashHall.bmhID, BeitMidrashHall.name
-ORDER BY BeitMidrashHall.bmhID
