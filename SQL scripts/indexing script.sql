67d07bd0fb85b7c6b209cf090439648eccc51168
YurishoSan <yurisho@gmail.com>
2015-06-03 19:04:18 +0300

    Indexing
    + added Indexing script (don't run until times measured)
    * fixed query2 to be faster

---
+++ SQL scripts/indexing script.sql
@@ -0,0 +1,8 @@
+CREATE INDEX shelfNum_idx
+ON Book (shelfnum);
+
+CREATE INDEX category_idx
+ON Book (category);
+
+CREATE INDEX capacity_idx
+ON BeitMidrashHall (capacity)
--- SQL scripts/query2_ShelvesByBooks.sql
+++ SQL scripts/query2_ShelvesByBooks.sql
@@ -1,2 +1,3 @@
-WITH BookByShelf AS (SELECT shelfNum, COUNT(shelfNum) AS books FROM Book GROUP BY shelfNum HAVING COUNT(shelfNum) > 1)
-SELECT * FROM BookByShelf ORDER BY BookByShelf.books DESC
+SELECT shelfNum, COUNT(shelfNum) AS books FROM Book
+GROUP BY shelfNum HAVING COUNT(shelfNum) > 1
+ORDER BY books DESC
